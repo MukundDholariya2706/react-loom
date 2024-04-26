cc1899e8aae4701824a34fb6972af36b8427c397
Mukund Dholariya <mukunddtridhyatech@gmail.com>
2024-04-26 15:52:18 +0530

    loom install

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { setup } from "@loomhq/record-sdk";
+import { isSupported } from "@loomhq/record-sdk/is-supported";
+import { oembed } from "@loomhq/loom-embed";
+import { useEffect, useState } from "react";
+
+const PUBLIC_APP_ID = "865a41dd-32c0-48dc-b4b8-f9fc0ab13f61";
+const BUTTON_ID = "loom-record-sdk-button";
 
 function App() {
+  const [videoHTML, setVideoHTML] = useState("");
+
+  useEffect(() => {
+    async function setupLoom() {
+      const { supported, error } = await isSupported();
+
+      if (!supported) {
+        console.warn(`Error setting up Loom: ${error}`);
+        return;
+      }
+
+      const button = document.getElementById(BUTTON_ID);
+
+      if (!button) {
+        return;
+      }
+
+      const { configureButton } = await setup({
+        publicAppId: PUBLIC_APP_ID,
+      });
+
+      const sdkButton = configureButton({ element: button });
+
+      sdkButton.on("insert-click", async (video) => {
+        const { html } = await oembed(video.sharedUrl, { width: 400 });
+        setVideoHTML(html);
+      });
+    }
+
+    setupLoom();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button id={BUTTON_ID}>Record</button>
+        <div dangerouslySetInnerHTML={{ __html: videoHTML }}></div>
       </header>
     </div>
   );
