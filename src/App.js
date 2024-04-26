23e59304958e8777bea52a5aa0f567fc6bb06520
Mukund Dholariya <mukunddtridhyatech@gmail.com>
2024-04-26 16:42:44 +0530

    update id

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 
 import { setup } from "@loomhq/record-sdk";
-import { isSupported } from "@loomhq/record-sdk/is-supported";
 import { oembed } from "@loomhq/loom-embed";
 import { useEffect, useState } from "react";
+import { isSupported } from "@loomhq/record-sdk/is-supported";
 
+// const PUBLIC_APP_ID = "865a41dd-32c0-48dc-b4b8-f9fc0ab13f61";
 const PUBLIC_APP_ID = "865a41dd-32c0-48dc-b4b8-f9fc0ab13f61";
 const BUTTON_ID = "loom-record-sdk-button";
 
